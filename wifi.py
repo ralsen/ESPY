b6e93bc9c93e89b71029edb28a7541fd605c5eba
ralsen <follrichs@gmx.de>
2023-12-20 17:04:56 +0100

    cosmetics

--- wifi.py
+++ wifi.py
@@ -10,23 +10,22 @@ LED_Pin = Pin(2, Pin.OUT)
 def connectBlink(timer):
     LED_Pin.value(not LED_Pin.value())
 
-blink = TM.freeTimer("Blinker", 250, connectBlink)
-
 def do_connect(SSID, Passw):
-    blink = TM.freeTimer("Blinker", 100, connectBlink)
+    blink = TM.freeTimer("WLAN-Blinker", 100, connectBlink)
     blink.start()
     wlan = network.WLAN(network.STA_IF)
     wlan.active(True)
     if not wlan.isconnected():
-        print(f'\n\r\nconnecting to network {SSID} ', end='')
+        print(f'\n\rconnecting to network {SSID} - {Passw}', end='')
         TM.downTimers.downCnters["WLAN"] = 3000
         wlan.connect(SSID, Passw)
         while not wlan.isconnected():
             print(".", end='')
             time.sleep(0.1)
             if(TM.downTimers.downCnters["WLAN"] == 0):
+                print(" ")
                 blink.stop()
                 return None
+    print(" ")
     blink.stop()
     return(wlan.ifconfig())
-
